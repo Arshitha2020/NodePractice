290cf11c5296b1b5d1b6061fdca14c154d6252a2
Arshitha2020 <arshitha.s.a@sap.com>
2022-03-12 00:16:30 +0530

    commit 3

--- practice/index.js
+++ practice/index.js
@@ -1,1 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+fs.readFile("filee.txt", "utf-8", (err, data) => {
+  const data1 = data;
+  console.log(data1);
+});
+
+const server = http.createServer(function (req, res) {
+  const { query, pathName } = url.parse(req.url, true);
+  //res.write(query)
+
+  res.writeHead(200, { "Content-type": "text/plain" });
+  res.end("data");
+});
+server.listen(8090, () => {
+  console.log("Listenening to requests on port 8080"); // we started to listen to incoming requests on local host IP and port
+});
+
 console.log("You ran");
+//console.log(name);
+const fun = function (name) {
+  return name;
+};
+
+const funny = (name) => {
+  return name;
+};
+
+console.log(funny("joe"));
